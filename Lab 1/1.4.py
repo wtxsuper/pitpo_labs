4eacdf58ca5f174eb3707305e1c4ea3a8b48bd09
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2024-02-04 19:35:37 +0600

    Init commit

---
+++ Lab 1/1.4.py
@@ -0,0 +1,43 @@
+# блоки голосования
+blocks = int(input())
+input()  # пропуск строки ввода
+for _ in range(blocks):
+    names = {}  # имена кандидатов в порядке бюллетеня
+    papers = []
+    c_num = int(input())
+    # Ввод кандидатов
+    for i in range(c_num):
+        names[str(i + 1)] = input()
+    # Ввод бюллетеней
+    while True:
+        t = input()
+        if t.strip() == "":
+            break
+        papers.append(t.split())
+
+    while True:
+        candidates = {k: 0 for k in names.keys()}
+        for paper in papers:
+            candidates[paper[0]] += 1
+        # Сортировка словаря по значениям
+        sorted_candidates = sorted(candidates.items(), key=lambda x: x[1])
+
+        # -- Условия победы --
+        # Если кандидат с наибольшим кол-вом голосов имеет 50 и более процентов
+        if sorted_candidates[-1][1] / len(papers) > 0.5:
+            print(names[sorted_candidates[-1][0]])
+            break
+        is_equal = True
+        for i in range(len(sorted_candidates) - 1):
+            if sorted_candidates[i][1] != sorted_candidates[i + 1][1]:
+                is_equal = False
+                break
+        # Если равное кол-во голосов - вывод всех кандидатов
+        if is_equal:
+            for sc in sorted_candidates:
+                print(names[sc[0]])
+            break
+        # -- Убираем из бюллетеней наименьшего --
+        smaller = sorted_candidates[0][0]
+        for paper in papers:
+            paper.remove(smaller)
